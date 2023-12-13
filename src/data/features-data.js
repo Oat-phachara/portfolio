87788831ed01d7ff821971fabfe999bc4d61d5ba
phachara <oat19411@hotmail.com>
2023-12-13 14:31:36 +0700

    version 1.0

---
+++ src/data/features-data.js
@@ -0,0 +1,29 @@
+import {
+  ChatBubbleBottomCenterTextIcon,
+} from "@heroicons/react/24/solid";
+
+export const featuresData = [
+  {
+    color: "gray",
+    title: "Software engineering projects",
+    icon: ChatBubbleBottomCenterTextIcon,
+    description:
+      "Demonstrating my ability to build robust and functional applications.",
+  },
+  {
+    color: "gray",
+    title: "UI/UX design projects",
+    icon: ChatBubbleBottomCenterTextIcon,
+    description:
+      "showcasing my talent for creating intuitive and user-friendly interfaces.",
+  },
+  {
+    color: "gray",
+    title: "Creative projects",
+    icon: ChatBubbleBottomCenterTextIcon,
+    description:
+      "Highlighting my ability to think outside the box and translate ideas into unique and innovative solutions.",
+  },
+];
+
+export default featuresData;
